0f052d68f9d46b12f3867192d1a5058610698b1e
Matthew Weier O'Phinney <matthew@zend.com>
2015-02-17 19:56:04 +0000

    Importing state as of zendframework/zf2@a24f638c30145cdcc58658085bd1ad6911012fe8 (1424202964)
    Automatic import via rsync

--- Protocol/Smtp/Auth/Crammd5.php
+++ Protocol/Smtp/Auth/Crammd5.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -22,10 +22,12 @@ class Crammd5 extends Smtp
      */
     protected $username;
 
+
     /**
      * @var string
      */
     protected $password;
+
 
     /**
      * Constructor.
@@ -62,6 +64,7 @@ class Crammd5 extends Smtp
         // Call parent with original arguments
         parent::__construct($host, $port, $origConfig);
     }
+
 
     /**
      * Performs CRAM-MD5 authentication with supplied credentials
